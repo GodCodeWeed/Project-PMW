0e7c515a51a0b3e8f8749167c8082f074b360bf5
holaryiwola <wasiuodesanya@gmail.com>
2022-10-26 20:15:12 -0500

    images

--- lib/Model/model_slide.dart
+++ lib/Model/model_slide.dart
@@ -13,23 +13,19 @@ class SlideModel {
 
 final slideList = [
   SlideModel(
-      imageUrl: 'assets/Images/LandingPage1.jpg',
+      imageUrl: 'assets/Images/image1.jpg',
       title: 'A Land Of Opputunity.',
       description:
           'Make your dreams come alife. This  is platform created for poeple with the fire and the hustle in them to succeed. Join hustle can partner with like minds inyour field to make money on the go.'),
   SlideModel(
-      imageUrl: 'assets/Images/LandingPage2.jpg',
+      imageUrl: 'assets/Images/image2.jpg',
       title: 'Get anything done.',
       description:
           'Looking for a Job to get done? not worry at Hustle we have thousands of expirienced professionals, who can get the job done in a jiphy.'),
   SlideModel(
-      imageUrl: 'assets/Images/imageme.jpeg',
+      imageUrl: 'assets/Images/image3.jpeg',
       title: 'Show Us your Hustle.',
       description:
           'The world has been waiting for your hustle now the time for you show it to the whole world, whatever it is, theres somoene out there in need of your services.'),
-  SlideModel(
-      imageUrl: 'assets/Images/imageme.jpeg',
-      title: 'Let your hustle TALK.',
-      description:
-          'Looking for a Job to get done? not worry at Hustle we have thousands of expirienced professionals, who can get the job done in a jiphy.'),
+
 ];
