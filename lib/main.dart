d5804cdfaf065581ee3ebf9b3414874542d2711b
holaryiwola <wasiuodesanya@gmail.com>
2022-10-26 20:13:18 -0500

    image changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'Utility/DependencyInjection/dependencyInjection.dart';
 import 'Utility/ScreenNavigation/screenNavigation_functions.dart';
 import 'Utility/ScreenNavigation/screenNavigation_names.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+//git config --global user.name "holaryiwola"
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
