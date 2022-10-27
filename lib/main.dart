483ccc91cde23851f4d2d63a47ecf9051b0fcc41
Nwamina Philip <nwamina.philip@gmail.com>
2022-10-26 21:04:00 -0500

    logic added to login screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,139 @@
 import 'package:flutter/material.dart';
-import 'UI/Screens/screen_landing.dart';
-import 'UI/Screens/screen_login.dart';
-import 'UI/Screens/screen_signup.dart';
-import 'UI/Themes&Colors/theme_custom.dart';
-import 'Utility/DependencyInjection/dependencyInjection.dart';
-import 'Utility/ScreenNavigation/screenNavigation_functions.dart';
-import 'Utility/ScreenNavigation/screenNavigation_names.dart';
-import 'package:firebase_core/firebase_core.dart';
-//git config --global user.name "holaryiwola"
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
- 
- setupLocator();
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../Data/data_firebase_authentication_service.dart';
+import '../../Logic/logic_firebase_login.dart';
+import '../../Utility/DependencyInjection/dependencyInjection.dart';
+import '../../Utility/ScreenNavigation/screenNavigation_functions.dart';
+import '../../Utility/ScreenNavigation/screenNavigation_names.dart';
+import '../Texts/text_custom.dart';
+import '../Widgets/AppBar/appbar_basic.dart';
+import '../Widgets/Buttons/button_raised.dart';
+import '../Widgets/Icons/icon_custom.dart';
+import '../Widgets/TextFields/textfield_custom.dart';
 
-  runApp(const MyApp());
-}
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  var dataLayer = locator<FireBaseAuthentication>();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: ScreenNavigationFunctions.instance.navigationKey,
-      initialRoute: ScreeNavigationNames.landingScreen,
-      routes: {
-ScreeNavigationNames.loginScreen:(BuildContext context) =>LoginScreen(),
-ScreeNavigationNames.signupScreen:(BuildContext context) =>SignUpScreen(),
-ScreeNavigationNames.landingScreen:(BuildContext context) =>LandingScreen(),
-      },
-      theme: CustomTheme.lightTheme,
-      debugShowCheckedModeBanner: false,
-      home: LandingScreen(),
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: BasicAppBar(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: BlocProvider(
+          create: (context) => LoginFirebaseLogic(dataLayer),
+          child: BlocConsumer<LoginFirebaseLogic, LoginFirebaseBaseState>(
+            listener: (context, state) {
+             
+            },
+            builder: (context, state) {
+              return Container(
+                  height: screenHeight,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.infinity,
+                  child: Center(
+                    child: SingleChildScrollView(
+                      child: Form(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  "Jobs wipes",
+                                  style: CustomTextStyle.logoText,
+                                )), //epiesboj
+                            SizedBox(height: 20),
+                            CustomTextfield(
+                              controller: emailController,
+                              hint: "Email",
+                              obscure: false,
+                              icon: SizedBox.shrink(),
+                            ),
+                            SizedBox(height: 10),
+                            CustomTextfield(
+                                controller: passwordController,
+                                hint: "Password",
+                                obscure: true,
+                                icon: CustomIcon(
+                                  icon: Icons.visibility_off,
+                                )),
+                            SizedBox(height: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Remember me',
+                                        textAlign: TextAlign.right,
+                                      ),
+                                    ),
+                                    Switch(
+                                      value: false,
+                                      onChanged: (value) {},
+                                      activeTrackColor: Colors.lightGreenAccent,
+                                      activeColor: Colors.green,
+                                    )
+                                  ],
+                                ),
+                                new Container(
+                                  padding:
+                                      const EdgeInsets.only(right: 3, left: 3),
+                                  child: ButtonRaised(
+                                    pressed: () {},
+                                    buttonTitle: "Login",
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: () {
+                                ScreenNavigationFunctions.instance.navigateTo(
+                                    ScreeNavigationNames.signupScreen);
+                              },
+                              child: SizedBox(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.transparent)),
+                                  child: Text.rich(TextSpan(
+                                      text: 'Dont have an account? ',
+                                      style:
+                                          CustomTextStyle.plainInformationText,
+                                      children: const <InlineSpan>[
+                                        TextSpan(
+                                          text: 'Sign Up',
+                                          style: CustomTextStyle
+                                              .coloredInformationText,
+                                        )
+                                      ])),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ));
+            },
+          ),
+        ),
+      ),
     );
   }
 }
-
-
